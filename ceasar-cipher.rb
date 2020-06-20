9b4a6578d321a8503c175fde5ab9693af585904a
Christian Robinson <chrisdrobbo@gmail.com>
2020-06-20 11:41:02 +0100

    Ceasar Cipher challenge completed!

--- ceasar-cipher.rb
+++ ceasar-cipher.rb
@@ -1,33 +1,42 @@
-# array which stores the alphabet
-
-# function which accepts two arguments. First is the string, second is the integercipher shift). 
-# Finds the index of inputted letter, adds the cipher shift string storing it as x
-# IF x is greater than > 26, retrun value at index x - 26
-# ELSE returns value at index x
-
-
 def ceasar_cipher(string, number)
     alphabet_array_lower = [*('a'..'z')]
     alphabet_array_upper = [*('A'..'Z')]
-    split_string = string.downcase!.split(//)
-    
-        cipher_index_array = split_string.map do |n, i | 
+    special_char_array = [*('!'..'?')]
+    number_check = cipher_large_numbers(number)
+    split_string = string.split(//)
+       
+    cipher_index_array = split_string.map do |n, i| 
 
-        if n === ' '
-            n             
-        elsif  (alphabet_array_lower.index(n) + number) > 26 
-            alphabet_array_lower[alphabet_array_lower.index(n) + number - 26]
-             
-        else
-            alphabet_array_lower[alphabet_array_lower.index(n) + number]
-        end 
-    end
+            if n === ' '
+                n      
+            elsif special_char_array.index(n)
+                n      
+            elsif  alphabet_array_upper.index(n) && (alphabet_array_upper.index(n) + number_check) > 26
+                alphabet_array_upper[alphabet_array_upper.index(n) + number_check - 26]
+            elsif alphabet_array_upper.index(n)
+                alphabet_array_upper[alphabet_array_upper.index(n) + number_check]   
+            elsif  (alphabet_array_lower.index(n) + number_check) > 26 
+                alphabet_array_lower[alphabet_array_lower.index(n) + number_check - 26] 
+            else
+                alphabet_array_lower[alphabet_array_lower.index(n) + number_check]
+            end 
+        end
     
-    ciphered_string = cipher_index_array.join('')
-    p ciphered_string
-    
+    cipher = cipher_index_array.join('')
+    p cipher
 end
 
-ceasar_cipher('What a string', 5)
+# Method to handle large cipher numbers
 
+def cipher_large_numbers(number) 
+
+    if number <= 26
+          number
+    else 
+       number = number % 26
+    end
+
+end
+
+ceasar_cipher('What a string!', 5)
 
